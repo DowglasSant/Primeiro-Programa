9e19153e50ace3a2cd2fee909559ca1c524bf97a
DowglasSant <dowglas.santana9@gmail.com>
2021-11-11 23:28:47 -0300

    Primeiro Programa em JAVA.

--- src/soma/PrimeiroPrograma.java
+++ src/soma/PrimeiroPrograma.java
@@ -5,7 +5,7 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) {
 			
 	int a = 13;
-	int b = 35;	
+	int b = 32;	
 
     System.out.println("A soma de A e B é: " + (b + a));
 
